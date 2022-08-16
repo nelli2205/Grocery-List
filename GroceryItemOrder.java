2be950722ab3bd4d27c92ec5b8333cc07f13ff42
nelli2205 <102549379+nelli2205@users.noreply.github.com>
2022-08-16 15:49:26 -0700

    Add files via upload

---
+++ GroceryItemOrder.java
@@ -0,0 +1,32 @@
+// This class stores information about a single grocery item being ordered.
+// Nelli Antonyan
+// BIT 142 SPRING 2022
+// A 4.1 revision 
+// 06.03.2022
+public class GroceryItemOrder {
+    private String name; // assigned variables to correct data type
+    private int quantity;
+    private double pricePerUnit;
+
+    public GroceryItemOrder(String name, int quantity, double pricePerUnit) { // constructor
+        this.name = name;
+        this.quantity = quantity;
+        this.pricePerUnit = pricePerUnit;
+    }
+
+    public void setQuantity(int quantity) { // changes the quantity
+        this.quantity = quantity;
+    }
+
+    public double getCost() { // returns the total cost
+        double totalCost;
+        totalCost = quantity * pricePerUnit; // used formula to find the cost for each item
+        return totalCost;
+    }
+
+    public String toString() { // returns a String representation of what the iteam and the quantity of this
+                               // item were
+        return (quantity + " of " + name);
+    }
+
+}
